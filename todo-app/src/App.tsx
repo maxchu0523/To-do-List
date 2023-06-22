84f48513d1a29bbc4fc3da70d8e4d6ca18198196
maxchu0523 <47725098+maxchu0523@users.noreply.github.com>
2023-06-22 14:31:25 -0400

    init commit

---
+++ todo-app/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+import ToDoList from './components/TodoList'
+function App() {
+
+  return (
+    <>
+      <ToDoList></ToDoList>
+    </>
+  )
+}
+
+export default App
---
+++ todo-app/src/interface.tsx
@@ -0,0 +1,6 @@
+export interface ITodoItem {
+    id: string;
+    todo: string;
+    completed: boolean;
+  }
+  
